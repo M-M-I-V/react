d63052d428c1eb7c419e9eee9409de3381d76319
M-M-I-V <neiljaylacandazo@gmail.com>
2025-03-15 23:10:36 +0800

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import Welcome from './components/Welcome'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
