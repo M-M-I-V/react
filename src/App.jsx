8dad106bf3423c19a0edbff8f7c10e9cd202bfcf
M-M-I-V <neiljaylacandazo@gmail.com>
2025-03-16 11:01:03 +0800

    Named Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './index.css'
 import Welcome from './components/Welcome.jsx'
 import Greeting from './components/Greeting.jsx'
-
+import { FirstComponent, SecondComponent } from './components/MyComponent.jsx'
+import MyComponent from './components/MyComponent.jsx'
 
 const App = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <Welcome library = "React"/>
       <Greeting name = "Neil"/>
+      <FirstComponent />
+      <SecondComponent />
+      <MyComponent />
     </div>
   )
 }
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,25 @@
+export const FirstComponent = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center">First Named Component</h1>
+        </div>
+    )
+}
+
+export const SecondComponent = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center">Second Named Component</h1>
+        </div>
+    )
+}
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center">Default Component</h1>
+        </div>
+    )
+}
+
+export default MyComponent
